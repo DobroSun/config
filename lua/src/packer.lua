e8c52348c2c252acb86603838285a8f65b8e6311
Dobrosun <mr.dobrogost02@mail.ru>
2024-02-09 12:57:56 +0300

    nvim: Initial setup

---
+++ lua/src/packer.lua
@@ -0,0 +1,18 @@
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('mbbill/undotree')
+  use('ellisonleao/gruvbox.nvim')
+end)
+
